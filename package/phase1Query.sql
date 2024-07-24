d60dbeaa8f4429a18ca936546c95b465bcefc4ff
Tuneer Mahatpure <mahatpuretuneer@gmail.com>
2024-07-24 13:15:58 +0530

    updated sql
    updated sql file

--- package/phase1Query.sql
+++ package/phase1Query.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT DISTINCT
     a.`Geography (Province name)`,
     a.`Geography (CMA name)`,
     a.`Geography (CA name)`,
@@ -6,8 +6,12 @@ SELECT
     a.`Year`,
     a.`Percentage of families (Total Income)`,
     a.`Percentage of families (After-tax income)`,
+    a.`Number of families (Total Income)`,
+    a.`Number of families (After-tax income)`,
     b.`Type of House`,
-    b.`Type of Bedroom`
+    b.`Type of Bedroom`,
+    INCOME_RANKING_CMA.`Ranking`,
+    b.`RentValue`
 FROM
     `chec_db`.`Canadian_Income_Survey` a
 JOIN
@@ -21,5 +25,9 @@ JOIN
     AND TRIM(a.`Geography (CMA name)`) = TRIM(c.`Geography (CMA name)`)
     AND TRIM(a.`Year`) = TRIM(c.`Year`)
     AND TRIM(b.`Type of House`) = TRIM(c.`Type of House`)
+JOIN
+    `chec_db`.`Income_Ranking_CMA` INCOME_RANKING_CMA
+    ON TRIM(a.`Geography (CMA name)`) = TRIM(INCOME_RANKING_CMA.`Major CMA`)
+    AND TRIM(a.`Year`) = TRIM(c.`Year`)
 WHERE
     TRIM(a.`Geography (Province name)`) = "Alberta" AND TRIM(a.`Geography (CMA name)`) = "Edmonton" AND TRIM(a.`Year`) = 2021 AND TRIM(c.`Type of House`) = "Apartment";
